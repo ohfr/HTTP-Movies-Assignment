9fff5658c0613164ee8f6b4d8ad0024889f666a1
Daniel Martin <danielmartin1205@gmail.com>
2019-11-07 19:35:34 -0500

    added mvp functions

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import MovieCard from "./MovieCard";
+
 export default class Movie extends React.Component {
   constructor(props) {
     super(props);
@@ -31,6 +32,24 @@ export default class Movie extends React.Component {
     addToSavedList(this.state.movie);
   };
 
+  editMovie = () => {
+    this.props.history.push(`/update-movie/${this.props.match.params.id}`)
+  }
+
+  deleteMovie = () => {
+    if(window.confirm('Are you sure you want to Delete this movie ?')){
+      axios.delete(`http://localhost:5000/api/movies/${this.props.match.params.id}`)
+        .then(res => {
+          console.log('Deleted Movie')
+          this.setState({
+            movie: null
+          })
+          this.props.history.push('/')
+        })
+        .catch(err => console.log(err))
+    }
+  }
+
   render() {
     if (!this.state.movie) {
       return <div>Loading movie information...</div>;
@@ -39,6 +58,8 @@ export default class Movie extends React.Component {
     return (
       <div className="save-wrapper">
         <MovieCard movie={this.state.movie} />
+        <div className="edit-button" onClick={this.editMovie}>Edit</div>
+        <div className="delete-button" onClick={this.deleteMovie}>Delete</div>
         <div className="save-button" onClick={this.saveMovie}>
           Save
         </div>
